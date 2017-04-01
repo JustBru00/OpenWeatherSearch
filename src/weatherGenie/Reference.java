062b415ac532cbe9cbece86239169cb1f0c7f378
Justin Brubaker <justbru00@gmail.com>
2017-04-01 13:08:43 -0400

    Change favorite to new york

--- src/weatherGenie/Reference.java
+++ src/weatherGenie/Reference.java
@@ -8,7 +8,7 @@ package weatherGenie;
  *
  */
 public class Reference {
-	public static final String favoriteQuery = "17543";
+	public static final String favoriteQuery = "10001";
 	
 	public static final String openWeatherAppId = "a880132d2e4609e673bee2b90972c170";
 	
